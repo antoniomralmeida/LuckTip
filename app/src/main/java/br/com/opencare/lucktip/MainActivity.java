201688504bf120ac30f8d76063dcd0fc3cf07481
Manoel Ribeiro <manoel.ribeiro@lsbd.ufc.br>
2017-04-11 15:31:14 -0300

    test bit bucket

--- app/src/main/java/br/com/opencare/lucktip/MainActivity.java
+++ app/src/main/java/br/com/opencare/lucktip/MainActivity.java
@@ -305,7 +305,6 @@ public class MainActivity extends AppCompatActivity {
             super(fm);
         }
 
-
         @Override
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
